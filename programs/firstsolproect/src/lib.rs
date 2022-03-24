a79b99efbb68b00fc8da99d5f45c318afbf046ab
Muskulee <muskulee@gmail.com>
2022-03-24 02:42:04 +0100

    V1.1

--- programs/firstsolproect/src/lib.rs
+++ programs/firstsolproect/src/lib.rs
@@ -5,10 +5,47 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod firstsolproect {
   use super::*;
-  pub fn start_stuff_off(_ctx: Context<StartStuffOff>) -> Result<()> {
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result<()> {
+    // Get a reference to the account.
+    let base_account = &mut ctx.accounts.base_account;
+    // Initialize total_gifs.
+    base_account.total_gifs = 0;
     Ok(())
   }
+
+
+  	// Another function woo!
+      pub fn add_gif(ctx: Context<AddGif>) -> Result<()> {
+        // Get a reference to the account and increment total_gifs.
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
+        Ok(())
+      }
+
+
 }
 
+// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
